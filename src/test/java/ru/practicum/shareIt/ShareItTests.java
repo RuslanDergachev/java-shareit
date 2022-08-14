fd5dae952ac7a7abed8eb73d94109694673bb3bb
Руслан Дергачев <dergrus@yandex.ru>
2022-08-14 16:24:50 +0300

    Поменял настройки на H2

---
+++ src/test/java/ru/practicum/shareIt/ShareItTests.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareItTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
